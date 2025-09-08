faf45795e945e3d4ccf4adc060851cb4423f4089
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2025-09-08 07:31:48 +0200

    Replace kapt with ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   alias(libs.plugins.kotlin) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.kotlin.serialization) apply false
-  alias(libs.plugins.kotlin.kapt) apply false
+  alias(libs.plugins.ksp) apply false
   alias(libs.plugins.hilt) apply false
 }
 
