1ef1a553ee67ea9dfc0dd6fc4e40eedf59e38aca
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2025-09-19 20:31:22 +0200

    Persist selected languages (#42)

--- feature/settings/impl/build.gradle.kts
+++ feature/settings/impl/build.gradle.kts
@@ -20,6 +20,7 @@ android {
 dependencies {
   implementation(project(":feature:settings:api"))
   implementation(project(":core:common"))
+  implementation(libs.androidx.datastore.preferences)
 
   implementation(libs.hilt.android)
   ksp(libs.hilt.compiler)
