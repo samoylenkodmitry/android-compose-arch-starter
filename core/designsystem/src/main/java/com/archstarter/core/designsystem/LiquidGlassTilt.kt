d51b730c4fd8cda0c26101f110ca517ee2b24baf
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2025-09-27 20:46:39 +0300

    looks better

--- core/designsystem/src/main/java/com/archstarter/core/designsystem/LiquidGlassTilt.kt
+++ core/designsystem/src/main/java/com/archstarter/core/designsystem/LiquidGlassTilt.kt
@@ -1,17 +1,10 @@
 package com.archstarter.core.designsystem
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import android.content.Context
 import android.view.Surface
 import android.view.WindowManager
 import androidx.compose.runtime.Composable
